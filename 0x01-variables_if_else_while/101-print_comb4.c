90a7541c00ad40481c64ab4e93e34f0250cca3a1
kidusmik <kidusmik@gmail.com>
2021-08-27 14:09:47 -0700

    Modified: 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,23 +15,25 @@ int main(void)
 	{
 		for (num2 = 1; num2 <= 8; num2++)
 		{
-				for (num3 = 2; num3 <= 9; num3++)
-				{
+			for (num3 = 2; num3 <= 9; num3++)
+			{
 				if ((num1 < num2) && (num2 < num3))
 				{
-				putchar(num1 + '0');
-				putchar(num2 + '0');
-				putchar(num3 + '0');
-				if (num1 < 7)
-				{
-				putchar(',');
-				putchar(' ');
-				}
-				}
-				}
-				}
-				}
-				putchar('\n');
+					putchar(num1 + '0');
+					putchar(num2 + '0');
+					putchar(num3 + '0');
 
-				return (0);
+					if (num1 < 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+
+				}
+
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
