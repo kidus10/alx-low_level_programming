20abc3b3c81938b2fc8cea5af730e22db56cc1dd
kidusmik <kidusmik@gmail.com>
2021-08-31 13:12:49 +0000

    Added: 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_alphabet - print the alphabet in lowercae followed by  a newline
+*
+* Return: void
+*/
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
+
