8cdb283e3cd2778a15010cc30666e586698eeb44
kidusmik <kidusmik@gmail.com>
2021-09-01 14:34:43 +0000

    added holberton.h

---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int x, int y, int z);
+
+#endif /* HOLBERTON_H */
