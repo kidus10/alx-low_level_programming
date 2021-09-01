81e8236f3485dd818a1715c5c087b6026b953c85
kidusmik <kidusmik@gmail.com>
2021-09-01 14:38:27 +0000

    modified holberton.h and 3-print_remaining_days.c

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -5,6 +5,6 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-void print_remaining_days(int x, int y, int z);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* HOLBERTON_H */
