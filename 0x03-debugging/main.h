2c029dc2ec02f95d4895e0c0aa7277c083fb6376
kidusmik <kidusmik@gmail.com>
2021-09-01 11:09:00 +0000

    Modified: 0-main.c and main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,8 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
-#include <stdlib.h>
 
-void positive_or_negative(int n);
+void positive_or_negative(int i);
 
-#endif /* _MAIN_H */
+#endif /* MAIN_H */
