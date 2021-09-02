ac4388939c0f592210ec93bbe3e19bb06d854d26
kidusmik <kidusmik@gmail.com>
2021-09-02 18:41:10 +0000

    added 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+* main - starting point of the program
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1 ; i <= 100 ; i++)
+	{
+		if (((i % 3) == 0) && ((i % 5) == 0))
+		{
+			printf("FizzBuzz ");
+			continue;
+		}
+		else if ((i % 3) == 0)
+		{
+			printf("Fizz ");
+			continue;
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz ");
+			continue;
+		}
+		printf("%d ", i);
+	}
+	printf("\n");
+}
