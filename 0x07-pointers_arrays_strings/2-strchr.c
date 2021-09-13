d03beda60363253337da4cde9a9ca04111f583a6
kidusmik <kidusmik@gmail.com>
2021-09-13 16:10:16 +0000

    added 2-strchr.c and modified main.h

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+/**
+* _strchr - locates a character in a string
+* @s: string
+* @c: character to be searched
+*
+* Return: char pointer
+*/
+char *_strchr(char *s, char c)
+{
+	while (*(++s) != '\0')
+	{
+		if (*s == c)
+			return (s);
+	}
+
+	return ((void *)0);
+}
