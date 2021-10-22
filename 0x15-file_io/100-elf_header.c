8ae1d3816cf9d71809bd2e5ed71ab2124be6350e
Kidus Worku <kidusmik@gmail.com>
2021-10-22 10:47:43 +0000

    Added: 100-elf_header.c

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+void exit_prog(char *message, int e_code, char *filename, int flag);
+int check_elf(char *elf_ptr);
+void print_magic(char *elf_ptr);
+void print_bit(char *elf_ptr);
+
+/**
+* main - displays the information contained in the ELF header at the start of an ELF file
+* @argc: argument count
+* @argv: argument list
+*
+* Return: Always 0
+*/
+int main(int argc, char **argv)
+{
+	int f_d, is_elf;
+	char *elf_header, *ptr_to_elf;
+	ssize_t r_bytes;
+
+	if (argc != 2)
+		exit_prog("Usage: elf_header elf_file", 98, NULL, 0);
+
+	elf_header = malloc(sizeof(char) * 64);
+	f_d = open(argv[1], O_RDONLY);
+	ptr_to_elf = elf_header;
+	
+	if (f_d == -1)
+		exit_prog("can't open the file", 98, argv[1], 1);
+
+	r_bytes = read(f_d, elf_header, 64);
+
+	if (r_bytes == -1)
+		exit_prog("can't read file", 98, argv[1], 1);
+
+	if (r_bytes < 5)
+		exit_prog("is not an ELF file", 98, argv[1], 2);
+
+	is_elf = check_elf(ptr_to_elf);
+
+	if (is_elf == 0)
+		exit_prog("is not an ELF file", 98, argv[1], 2);
+	else
+		print_magic(ptr_to_elf);
+
+	print_bit(ptr_to_elf);
+
+	return (0);
+}
+
+/**
+* exit_prog - exits the program with a message
+* @message: message to be printed
+* @e_code: exit code
+* @filename: the filename
+* @f_d: the file desccriptor
+* @flag: flags
+*
+* Return: Always void
+*/
+void exit_prog(char *message, int e_code, char *filename, int flag)
+{
+	if (flag == 0)
+		dprintf(STDERR_FILENO, "%s\n", message);
+	else if (flag == 1)
+		dprintf(STDERR_FILENO, "Error: %s %s\n", message, filename);
+	else if (flag == 2)
+		dprintf(STDERR_FILENO, "Error: %s %s\n", filename, message);
+	exit(e_code);
+}
+
+/**
+* check_elf - checks if it's elf
+* @ptr_elf - pointer to the elf header buffer
+*
+* Return: 1 if file is elf, otherwise 0
+*/
+int check_elf(char *elf_ptr)
+{
+	int i;
+	char e_h[4] = {0x7F, 0x45, 0x4c, 0x46};
+
+	i = 0;
+	while (i < 4)
+	{
+		if (e_h[i] != *(elf_ptr + i))
+			return (0);
+
+		i++;
+	}
+	return (1);
+}
+
+/**
+* print_magic - prints the magic of the elf filee
+* @elf_ptr: pointer to the elf header
+*
+* Return: Always void
+*/
+void print_magic(char *elf_ptr)
+{
+	int i;
+
+	i = 0;
+
+	printf("Magic:\t");
+	while (i < 16)
+	{
+		printf("%02x", *(elf_ptr + i));
+
+		if (i != 15)
+			printf(" ");
+
+		i++;
+	}
+	printf("\n");
+}
+
+void print_bit(char *elf_ptr)
+{
+	printf("Class:\t");
+	if (*elf_ptr == '1')
+		printf("ELF32");
+	else if (*elf_ptr == '2')
+		printf("ELF64");
+
+	printf("\n");
+}
