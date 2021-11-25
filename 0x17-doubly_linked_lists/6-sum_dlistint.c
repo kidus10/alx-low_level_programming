b7ab4c61a8449faabe13416e6fddc279439a96ec
Kidus Worku <kidusmik@gmail.com>
2021-11-25 16:37:47 +0000

    Fix style

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - returns the sum of all the data (n) of a dlistint_t linked list
+ * sum_dlistint - returns the sum of all the data\
+ *                 (n) of a dlistint_t linked list
  * @head: pointer to the head of the linked list
  *
  * Return: sum, otherwise 0
