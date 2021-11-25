948caa947eca9604e2700929dd0269e047b7d98d
Kidus Worku <kidusmik@gmail.com>
2021-11-25 15:42:47 +0000

    Modify 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,15 +11,11 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t count;
 
 	count = 0;
-
-	while (h->prev != NULL)
-		h = h->prev;
-
-	while (h->next != NULL)
+	while (h != NULL)
 	{
-		count++;
+		printf("%d\n", h->n);
 		h = h->next;
+		count++;
 	}
-
-	return (count + 1);
+	return (count);
 }
