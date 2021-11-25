987e344fae6a1de7c069f3d28e0f16a858992f0d
Kidus Worku <kidusmik@gmail.com>
2021-11-25 15:43:45 +0000

    Fix style

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
 * struct dlistint_s - doubly linked list
 * @n: integer
@@ -8,7 +11,7 @@
 * @next: points to the next node
 *
 * Description: doubly linked list node structure
-* 
+*
 */
 typedef struct dlistint_s
 {
@@ -17,5 +20,6 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
+size_t print_dlistint(const dlistint_t *h);
 
 #endif /* LISTS_H */
