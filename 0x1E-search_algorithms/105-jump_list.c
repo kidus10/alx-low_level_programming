2684f8686c6b0dbd60e79b305e8aa5047f092afb
kidusmik <kidusmik@gmail.com>
2022-03-31 09:11:50 +0000

    modify 105-jump_list.c

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -36,24 +36,29 @@ listint_t *jump_list_recurse_inner(listint_t *curr, size_t step, int val)
 }
 
 /**
- * jump_list_recurse - recursive implement for recursive implement
- * @curr: current node to check
- * @step: interval of jumps
+ * jump_list_recurse - recursive implement of jump search
+ * @curr: current node of list
+ * @size: size of list
+ * @step: jump increment
  * @val: search value
  *
  * Return: index where value is located; -1 if value not found
  */
-listint_t *jump_list_recurse(listint_t *curr, size_t step, int val)
+listint_t *jump_list_recurse(listint_t *curr, size_t size,
+		size_t step, int val)
 {
-	if (curr == NULL || step <= 0 || curr->n > val)
-		return (NULL);
+	listint_t *next_step = NULL;
 
-	printf("Value checked at index [%lu] = [%d]\n", curr->index, curr->n);
+	next_step = look_ahead(curr, step);
+	printf("Value checked at index [%lu] = [%d]\n",
+next_step->index, next_step->n);
+	if (curr->index + step < size && (next_step->n < val))
+		return (jump_list_recurse(next_step, size, step, val));
 
-	if (curr->n == val)
-		return (curr);
-	else
-		return (jump_list_recurse_inner(curr->next, step - 1, val));
+	printf("Value found between indexes [%lu] and [%lu]\n",
+curr->index, (next_step->index));
+
+	return (jump_list_recurse_inner(curr, step, val));
 }
 
 /**
